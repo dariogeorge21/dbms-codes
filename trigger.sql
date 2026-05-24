cbaf9414abe843651321e10d2cdcf808025bde27
Dario George <edu.dariogeorge21@gmail.com>
2026-05-25 00:18:47 +0530

    Refactor triggers to update student grades and enforce minimum mark constraint

--- trigger.sql
+++ trigger.sql
@@ -17,7 +17,9 @@ CREATE TRIGGER update_grade
 AFTER INSERT ON STUDENTS
 FOR EACH ROW
 BEGIN
-    SET NEW.mark = NEW.mark + 5;
+    UPDATE STUDENTS
+    SET mark = mark + 5
+    WHERE id = NEW.id;
 END;
 
 -- Example: Creating a log table and a trigger to log deletions from the STUDENTS table
@@ -37,4 +39,16 @@ FOR EACH ROW
 BEGIN
     INSERT INTO student_logs (name, log_time)
     VALUES (CONCAT('Deleted student ', OLD.name), CURRENT_TIME());
+END;
+
+-- Example: Creating a trigger to prevent updates to the mark column if the new mark is less than 50
+
+CREATE TRIGGER check_mark
+BEFORE INSERT
+ON STUDENTS
+FOR EACH ROW
+BEGIN
+    IF NEW.mark < 50 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Mark must be at least 50';
+    END IF;
 END;
